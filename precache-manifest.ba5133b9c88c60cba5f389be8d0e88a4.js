9ecf6f12bedac384e4602edbe5eca4dbcceb5ec7
Taufiq Dwi Cahyono <taufiqdwic@gmail.com>
2020-10-15 01:28:12 +0700

    Updates

---
+++ precache-manifest.ba5133b9c88c60cba5f389be8d0e88a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89d6116f8a8ba9011eef9f7437d6cad1",
+    "url": "/mycv/index.html"
+  },
+  {
+    "revision": "23cb0581332b1519c76a",
+    "url": "/mycv/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "42e4253a23124092d67d",
+    "url": "/mycv/static/js/2.8d669ae3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mycv/static/js/2.8d669ae3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23cb0581332b1519c76a",
+    "url": "/mycv/static/js/main.11782f7d.chunk.js"
+  },
+  {
+    "revision": "9c9156fde34d30785013",
+    "url": "/mycv/static/js/runtime-main.f7b5f11b.js"
+  }
+]);
